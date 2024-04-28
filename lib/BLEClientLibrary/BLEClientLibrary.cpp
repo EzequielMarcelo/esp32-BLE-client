fb260a8b1100d8b55a02e2746d3328b9b7d294ea
Marcelo Gustavo <marcelogustavo05@hotmail.com>
2024-04-28 12:12:31 -0300

    ADDED: Stored the found devices.
    Closes #2.

--- lib/BLEClientLibrary/BLEClientLibrary.cpp
+++ lib/BLEClientLibrary/BLEClientLibrary.cpp
@@ -8,11 +8,14 @@ void BLEClientLibrary::begin()
     _scan->setInterval(1349);
     _scan->setWindow(449);
     _scan->setActiveScan(true);
-    Serial.println("Scaneando...");
+    Serial.println("Scanning...");
 }
 void BLEClientLibrary::StartScan()
 {
-    BLEDevice::getScan()->start(1);
+    BLEScanResults foundDevices = _scan->start(1);
+    Serial.print("Devices found: ");
+    Serial.println(foundDevices.getCount());
+    _scan->clearResults();
 }
 void BLEClientLibrary::onResult(BLEAdvertisedDevice device)
 {
